23c0982b5f70cdbd48148bac22fda648fb7d2ee2
darwinvasquez <jhondarwinvasq@gmail.com>
2017-11-14 21:29:22 -0500

    Plantilla home index

---
+++ sinfo/entidades/Areas/CodigoPolicia/CodigoPoliciaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace entidades.Areas.CodigoPolicia
+{
+    public class CodigoPoliciaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "CodigoPolicia";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "CodigoPolicia_default",
+                "CodigoPolicia/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ sinfo/entidades/Areas/ServicioWeb/ServicioWebAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace entidades.Areas.ServicioWeb
+{
+    public class ServicioWebAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ServicioWeb";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ServicioWeb_default",
+                "ServicioWeb/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
