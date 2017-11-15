0c2b02e1c1ab7260fb8d6fd507e578a44b3e2802
darwinvasquez <jhondarwinvasq@gmail.com>
2017-11-14 21:41:18 -0500

    Inicion

---
+++ sinfo/entidades/Areas/Conocimiento/ConocimientoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace entidades.Areas.Conocimiento
+{
+    public class ConocimientoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Conocimiento";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Conocimiento_default",
+                "Conocimiento/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
